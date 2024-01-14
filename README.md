8c69565a75935f9b2941d321506d482dca989fa9
AtumaDavid <atumadavid35@gmail.com>
2024-01-14 23:31:24 +0100

    my first comit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kaban Web App (React.js, Drag and Drop, MUI V5, Zustand)
+
+# Dependencies
+
+- npm install @mui/material @emotion/react @emotion/styled
+- npm install @mui/icons-material
+- react-router-dom
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "../theme";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  );
+}
+
+export default App;
